fdac61843adfb7fbb0779940d4f1796affec40c3
Nils Macher <n.macher@gmail.com>
2024-03-16 17:16:53 +0000

    update the app

--- breathworks/llm_logic/param.py
+++ breathworks/llm_logic/param.py
@@ -4,3 +4,5 @@ import os
 load_dotenv()
 
 ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY')
+pdf_file_path = os.path.join('documents','breathworks_guide.pdf')
+file_path = os.path.join('data','omfh_backup.csv')
