10e57737064ac57f8910dae70cc2d78f1e00f608
Jonny OHanlon <jonny_o_hanlon@hotmail.co.uk>
2024-03-21 21:41:07 +0000

    new branch

--- breathworks/utils.py
+++ breathworks/utils.py
@@ -2,16 +2,19 @@ import os
 from google.cloud import bigquery
 from dotenv import load_dotenv
 
-def get_data():
+def get_data(table_id):
     load_dotenv()
     PROJECT_ID = os.environ['GCP_PROJECT']
     DATASET_ID = os.environ['BQ_DATASET']
-    TABLE_DATA_ID = os.environ['BQ_TABLE_DATA']
 
     client = bigquery.Client()
 
+<<<<<<< HEAD
     query = f'SELECT * FROM `{PROJECT_ID}.{DATASET_ID}.{TABLE_DATA_ID}`'
     print(query)
+=======
+    query = f'SELECT * FROM `{PROJECT_ID}.{DATASET_ID}.{table_id}`'
+>>>>>>> 8d464e49bbd300bc51488aeb70bf3a96eb6d84ef
     query_job = client.query(query)
     df = query_job.to_dataframe()
     # print(query)
