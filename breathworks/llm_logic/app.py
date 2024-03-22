02bdd544aac1a221a7e124e909acac9c779a3df9
Nils Macher <n.macher@gmail.com>
2024-03-22 13:57:33 +0000

    finetune logic

--- breathworks/llm_logic/app.py
+++ breathworks/llm_logic/app.py
@@ -1,55 +1,90 @@
-from llama_index.llms.anthropic import Anthropic
-from llama_index.core.query_engine import PandasQueryEngine
-from llama_index.core import Settings
+# Standard Library
 import os
-import pandas as pd
-from llama_index.readers.file import CSVReader
-from pathlib import Path
-from llama_index.core import SimpleDirectoryReader, ServiceContext
-from llama_index.core.agent import ReActAgent
-from llama_index.core.tools import FunctionTool, QueryEngineTool, ToolMetadata
-from llama_index.core import Settings
-tokenizer = Anthropic().tokenizer
-from prompts import instruction_str, new_prompt, context
-from preprocessing import clean
-from param import ANTHROPIC_API_KEY
-from helpers import get_index
-import streamlit as st
 import requests
-from param import pdf_file_path, file_path
-from llama_index.readers.file import PDFReader
+from pathlib import Path
+
+# Local Files
+from helpers import get_index
+from preprocessing import clean
+from param import pdf_file_path, file_path, ANTHROPIC_API_KEY
+from prompts import instruction_str_pandas, instruction_str_llm, instruction_str_pdf, context, prompt_template_df, prompt_template_llm, prompt_template_pdf
+
+# Third part packages
+import pandas as pd
+import streamlit as st
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.anthropic import Anthropic
+from llama_index.readers.file import PDFReader, CSVReader
+from llama_index.core.query_engine import PandasQueryEngine
+from llama_index.core import Settings, SimpleDirectoryReader
+from llama_index.core.tools import FunctionTool, QueryEngineTool, ToolMetadata
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+
+# TODO
+# Review the prompts - we saw some of the prompts should be formatting and adding in an actual query but they aren't = new_prompt
+# Looking at the get_index function - doesn't always find the local index
+
 
 @st.cache_resource()
 def initialise():
     df = pd.read_csv(file_path)
-    df.Motivation.fillna('',inplace=True)
-
-    # Apply to all texts
+    df.Motivation = df.Motivation.fillna('')
     df['clean_text'] = df.Motivation
     llm = Anthropic(model="claude-3-opus-20240229")
-    # Settings.tokenizer = tokenizer
     Settings.llm = Anthropic(model="claude-3-opus-20240229",api_key=ANTHROPIC_API_KEY)
+    # Initial huggingface as embedding model
 
-    df_query_engine = PandasQueryEngine(df=df.head(),verbose=True,instruction_str=instruction_str)
-    df_query_engine.update_prompts({"pandas_prompt": new_prompt})
+    Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    tokenizer = Anthropic().tokenizer
+    Settings.tokenizer = tokenizer
+
+    # Load documents and create indexes
     documents = CSVReader().load_data(Path('data/df_head.csv'))
     pdf_documents = PDFReader().load_data(Path(pdf_file_path))
-    service_context = ServiceContext.from_defaults(chunk_size=1024, llm=llm, embed_model="local")
-    df_index = get_index(documents,'df_index',llm=llm,service_context=service_context)
-    pdf_index = get_index(pdf_documents,'pdf_index',llm=llm,service_context=service_context)
-    df_query_engine = PandasQueryEngine(df=df.head(),verbose=True,instruction_str=instruction_str)
-    df_query_engine.update_prompts({"pandas_prompt": new_prompt})
-    query_engine_llm = df_index.as_query_engine(llm=llm)
-    query_engine_pdf = pdf_index.as_query_engine(llm=llm)
+
+
+    df_index = get_index(documents,'df_index',llm=llm)
+    pdf_index = get_index(pdf_documents,'pdf_index',llm=llm)
+
+    # Create query engines
+    df_query_engine = PandasQueryEngine(df=df.head(),verbose=True,instruction_str=instruction_str_pandas)
+    query_engine_llm = df_index.as_query_engine(llm=llm,instruction_str=instruction_str_llm)
+    query_engine_pdf = pdf_index.as_query_engine(llm=llm, instruction_str=instruction_str_pdf)
+
+    # Create prompt templates with updated prompts
+
+    df_query_engine.update_prompts({
+    "pandas_prompt": prompt_template_df
+    })
+
+    # Update prompts for query_engine_llm
+    query_engine_llm.update_prompts({
+        "llm_prompt": prompt_template_llm
+    })
+
+    # Update prompts for query_engine_pdf
+    query_engine_pdf.update_prompts({
+        "pdf_prompt": prompt_template_pdf
+    })
+
+    # Update prompts for df_query_engine
+    df_query_engine.update_prompts({
+    "pandas_prompt": prompt_template_df
+    })
+
+    # Update prompts for query_engine_llm
+    query_engine_llm.update_prompts({
+        "llm_prompt": prompt_template_llm
+    })
+
+    # Update prompts for query_engine_pdf
+    query_engine_pdf.update_prompts({
+        "pdf_prompt": prompt_template_pdf
+    })
+
 
     tools = [
-        QueryEngineTool(
-            query_engine=df_query_engine,
-            metadata=ToolMetadata(
-                name="df_data",
-                description="this translates human lanuage into a pandas query",
-            ),
-        ),
         QueryEngineTool(
             query_engine=query_engine_llm,
             metadata=ToolMetadata(
@@ -67,27 +102,29 @@ def initialise():
     ]
 
     llm = Anthropic(model="claude-3-opus-20240229")
-    agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
+    agent = ReActAgent.from_tools(tools, llm=llm, verbose=True)
 
     '''
     # Breathworks Website
     '''
     return agent
+
+
 def main (agent):
     prompt = st.text_input('Prompt')
+    # prompt = 'what is the number 1 reason in 1 word why people join the class?'
     if prompt:
-        response = agent.chat(prompt)
+        response = agent.query(prompt)
+        print(response)
+        # for source in response.sources:
+        #     content = source.content
+        #     tool_name = source.tool_name
 
-        for source in response.sources:
-            # Assuming source is an instance of ToolOutput or a similar class
-            content = source.content
-            tool_name = source.tool_name
+        #     raw_input = source.raw_input['input']  # Assuming raw_input is a dictionary attribute
+        #     raw_output_response = source.raw_output.response  # As
 
-            # Accessing attributes of nested custom objects
-            raw_input = source.raw_input['input']  # Assuming raw_input is a dictionary attribute
-            raw_output_response = source.raw_output.response  # As
+        st.write(response.response)
 
-            st.write(raw_output_response)
 
 if __name__ == "__main__":
     agent = initialise()
